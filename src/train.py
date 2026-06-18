34e4b2ddd2f809fcf2aab124df292b2e75a991b6
BubbleGum-git <surajkumbharoistg7@gmail.com>
2026-06-18 18:36:43 +0530

    Added the missing SSIM from the Train function

--- src/train.py
+++ src/train.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torchvision.utils as vutils
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from pytorch_msssim import ssim as ssim_fn
 
 
 # ─────────────────────────────────────────────
